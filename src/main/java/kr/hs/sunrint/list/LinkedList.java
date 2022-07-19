a3e01e1083d9a30af5be76bcf32ebec215149b2d
haechilim <haechilim@gmail.com>
2022-07-19 13:39:23 +0900

    LinkedList 테스트 케이스 정의

---
+++ src/main/java/kr/hs/sunrint/list/LinkedList.java
@@ -0,0 +1,67 @@
+package kr.hs.sunrint.list;
+
+public class LinkedList {
+
+//    public boolean add(String element) {
+//
+//    }
+//
+//    public boolean add(int index, String element) {
+//
+//    }
+//
+//    public boolean addFirst(String element) {
+//
+//    }
+//
+//    public boolean addLast(String element) {
+//
+//    }
+//
+//    public String remove() {
+//
+//    }
+//
+//    public String removeFirst() {
+//
+//    }
+//
+//    public String removeLast() {
+//
+//    }
+//
+//    public String remove(int index) {
+//
+//    }
+//
+//    public boolean remove(String element) {
+//
+//    }
+//
+//    public String set(int index, String element) {
+//
+//    }
+//
+//    public String get(int index) {
+//
+//    }
+//
+//    public void clear() {
+//
+//    }
+//
+//    public int size() {
+//
+//    }
+//
+//    public boolean contains(String element) {
+//
+//    }
+//
+//    public int indexOf(String element) {
+//
+//    }
+
+
+
+}
---
+++ src/test/java/kr/hs/sunrint/list/LinkedListTest.java
@@ -0,0 +1,62 @@
+package kr.hs.sunrint.list;
+
+import kr.hs.sunrint.exception.IndexOutOfBoundException;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class LinkedListTest {
+    LinkedList list;
+
+    @BeforeEach
+    public void setup() {
+        list = new LinkedList();
+
+        list.add("abc");
+        list.add("def");
+        list.add("ghi");
+    }
+
+    @Test
+    public void 요소를_리스트_끝에_추가한다() {
+        assertEquals("abc", list.get(0));
+        assertEquals("def", list.get(1));
+        assertEquals(3, list.size());
+    }
+
+    @Test
+    public void 요소를_리스트_중간에_추가한다() {
+        list.add(1, "xyz");
+        assertEquals("abc", list.get(0));
+        assertEquals("xyz", list.get(1));
+        assertEquals("def", list.get(2));
+    }
+
+    @Test
+    public void 엾는_인덱스에_추가_하려고_하면_예외를_던진다() {
+        assertThrows(IndexOutOfBoundException.class, () -> list.add(4, "cat"));
+    }
+
+    @Test
+    public void 중간_요소를_삭제한다() {
+        list.remove("def");
+
+        assertEquals("ghi", list.get(1));
+    }
+
+    @Test
+    public void 마지막_요소를_삭제한다() {
+        list.remove(2);
+
+        assertEquals(2, list.size());
+    }
+
+    @Test
+    public void 리스트_초기화() {
+        list.clear();
+
+        assertEquals(0, list.size());
+    }
+}
